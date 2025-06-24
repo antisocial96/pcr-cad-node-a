1433ce0e909cabc5caf562bc28663fb05f5c8ab1
Ashwin <ashwin@auxill.ai>
2025-06-24 14:59:01 +0530

    Install Vite and ElevenLabs client

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>ElevenLabs Conversational AI</h1>
+    <p>Ready to integrate with ElevenLabs client</p>
+  </div>
+`
