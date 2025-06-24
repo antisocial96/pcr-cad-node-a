caa693cd7eb708eed5a6b07f33b74ba5d5f6a63b
Ashwin <ashwin@auxill.ai>
2025-06-24 13:54:06 +0530

    Updated package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'frontend',
-  build: {
-    outDir: '../dist'
-  },
   server: {
     port: 5173,
     strictPort: true
