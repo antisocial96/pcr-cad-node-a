a4388b4ca0e70b5f426e91eb1b3267d7670b40db
Ashwin <ashwin@auxill.ai>
2025-06-25 13:11:32 +0530

    Updated server.js

--- backend/test-server.js
+++ backend/test-server.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
