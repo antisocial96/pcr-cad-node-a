293a16810970e89c354f8549ed0e569c51deaea5
Ashwin <ashwin@auxill.ai>
2025-06-25 21:11:28 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import { garudaSentryCalls } from './utils/supabaseClient.js';
 config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT
 
 // Middleware
 app.use(cors());
