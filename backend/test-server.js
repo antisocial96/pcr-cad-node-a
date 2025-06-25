913b4a286a3dc9cbbe554dc8ba42173236dcf5c6
Ashwin <ashwin@auxill.ai>
2025-06-25 21:53:26 +0530

    Updated test-server.js

--- backend/test-server.js
+++ backend/test-server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('✅ Hello from test server!');
 });
 
-app.listen(PORT, '::', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on IPV6:${PORT}`);
 });
