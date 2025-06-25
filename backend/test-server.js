825fd7cd5266c369a6d03622619cc436018975c7
Ashwin <ashwin@auxill.ai>
2025-06-25 12:48:57 +0530

    Updated test-server.js

--- backend/test-server.js
+++ backend/test-server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('✅ Hello from test server!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
