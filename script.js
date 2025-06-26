eda3f0d9ba35b363079248bdb91f4543478d30b8
Ashwin <ashwin@auxill.ai>
2025-06-26 15:32:29 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ async function stopConversation() {
             setTimeout(() => {
                 //window.location.reload();
               fetchAndDisplayCalls();
-            }, 500);
+            }, 2000);
         } catch (error) {
             console.error('Error ending conversation:', error);
         }
